9c809e759d183cf7e2498cbb918b2ce24bcd8628
Jack <mythbusters@outlook.sg>
2022-10-03 13:24:54 +0800

    多省份类对象列表生成类

---
+++ src/main/java/org/Phoebej/provinces/Provinces.java
@@ -0,0 +1,25 @@
+package org.Phoebej.provinces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 身份信息集合对象
+ * @author Phoebej
+ * @date 2022/10/2
+ */
+public class Provinces {
+    private static ArrayList<ProvinceInfoGenerator> provincesList = new ArrayList<>();
+
+    static{
+        ProvinceInfoGenerator tj = new TianJin();
+        ProvinceInfoGenerator bj = new BeiJing();
+        provincesList.add(tj);
+        provincesList.add(bj);
+    }
+
+
+    public static ArrayList<ProvinceInfoGenerator> getProvincesList() {
+        return provincesList;
+    }
+}
