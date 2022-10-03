b51bff2241c1ac6c7a29c18d6b578a8716aabba5
Jack <mythbusters@outlook.sg>
2022-10-03 13:28:14 +0800

    将此类更改为说明父类

--- src/main/java/org/Phoebej/provinces/ProvincesInfo.java
+++ src/main/java/org/Phoebej/provinces/ProvincesInfo.java
@@ -1,6 +1,8 @@
 package org.Phoebej.provinces;
 
-import java.util.Map;
+
+import java.util.LinkedHashMap;
+
 
 /**
  * 所有省份类的父类，定义子类中需要初始化的成员属性
@@ -8,25 +10,27 @@ import java.util.Map;
  * @date 2022/10/1
  */
 public abstract class ProvincesInfo {
-    /*
-    省份信息
-     */
+    /**
+     *
+     * 省份信息
+     *
     // 省份名称
     static String provinceName;
     // 省份内包含的城市名称
     static String[] cityName;
     // 城市对应的区名称
-    static Map<String,String[]> districtName;
+    static LinkedHashMap<String,String[]> districtName;
     // 区对应的街道名称
     // static Map<String,String[]> blockName;
-    /*
-    身份证号地址码信息
-     */
+
+     * 身份证号地址码信息
+
     // 省份名称对应代码
     static Integer provinceId;
     // 城市名称对应的代码
-    static Map<String,String> cityId;
+    static LinkedHashMap<String,String> cityId;
     // 区所对应的代码
-    static Map<String,String> districtId;
+    static LinkedHashMap<String,String> districtId;
+    */
 
 }
